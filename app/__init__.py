dda89b7181f49fa3443e20577b68d8019bfc0dff
Aaron Contreras <aaroncontreras@dhcp-10-250-120-163.harvard.edu>
2022-12-04 18:20:45 -0500

    More API stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, app, render_template, redirect, request
+from cs50 import SQL
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -23,24 +24,79 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
+try:
+    results = sp.search(q='track:' + name, type='track')
 
+    items = results['tracks']['items']
 
+    if len(items) > 0:
+        track = items[0]
+        url = track['album']['images'][0]['url']
 
+    print(url)
 
-results = sp.search(q='track:' + name, type='track')
+except:
+    print("not a song")
 
-items = results['tracks']['items']
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
 
-if len(items) > 0:
-    track = items[0]
-    url = track['album']['images'][0]['url']
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return apology("must provide password", 400)
 
-print(url)
+        # Ensure confirmation password was submitted
+        if not request.form.get("confirmation"):
+            return apology("must provide confirmation", 400)
 
+        # Ensure confirmation password matches password
+        if request.form.get("confirmation") != request.form.get("password"):
+            return apology("passwords must match", 400)
+
+        # Ensure spotify username was submitted
+        if not request.form.get("spotifyuser"):
+            return apology("must provide Spotify username", 400)
+
+        '''
+        if not len(request.form.get("password")) > 8:
+            return apology("password must be greater than 8 characters")
+        '''
+        
+        #check if username is taken
+        rows1 = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(rows1) != 0:
+            return apology("username taken")
+
+        user = request.form.get("username")
+        hashpw = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+        spotify = request.form.get("spotifyuser")
+
+        try:
+            db.execute("INSERT INTO users (username, hash, spotify) VALUES (?, ?, ?)", user, hashpw, spotify)
+            return redirect("/")
+        except:
+            return apology("username taken", 200)
+    else:
+        return render_template("register.html")
 
 @app.route('/')
 def home():
     return render_template("template.html")
+
+
+@app.route('/post')
+def post():
+    return render_template("feed.html")
+
+
+@app.route('/feed')
+def feed():
+    return render_template("feed.html")
 
 
 
